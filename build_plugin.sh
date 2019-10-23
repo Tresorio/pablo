d82dea4f34dcdd3c83ac9b95c82abc8b4a59692d
Maxime CORBIN <maxime.corbin@epitech.eu>
2019-10-23 17:09:33 +0200

    fix build_plugin.sh

--- build_plugin.sh
+++ build_plugin.sh
@@ -1,8 +1,10 @@
+#!/usr/bin/env bash
+
 find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
 
 mkdir tresorio
 ln -s ../bundle_modules ./tresorio/bundle_modules
-ln -s ../config ./tresorio/config 
+ln -s ../config ./tresorio/config
 ln -s ../src ./tresorio/src
 ln -s ../__init__.py ./tresorio/__init__.py
 ln -s ../assets ./tresorio/assets
