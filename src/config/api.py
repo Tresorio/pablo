aa721113a17553c0610364bb9b59adbdd0299512
Robin <robin.christol@epitech.eu>
2019-10-30 10:24:40 +0000

    fix: now use an ssl_context for aiohttp to setup the certificates, using certifi

--- src/config/api.py
+++ src/config/api.py
@@ -2,6 +2,11 @@
 
 import src.utils.json_rw as json
 import src.config.paths as paths
+import ssl
+from bundle_modules import certifi
+
+SSL_CONTEXT = ssl.create_default_context()
+SSL_CONTEXT.load_verify_locations(certifi.where())
 
 IS_MOCKED = False
 API_CONFIG = json.load(paths.TRESORIO_CONFIG_PATH)
